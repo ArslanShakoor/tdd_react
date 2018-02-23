77c7d0c939140cd6a11da46291129cea48777150
Shakoor <arslan.shakoor@capgemini.com>
2018-02-23 16:51:32 -0600

    remove the test pollution by behaviour driver development

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -5,22 +5,34 @@ import App from './App';
 
 configure({ adapter: new Adapter() });
 
-const app = shallow(<App />);
+describe('App',()=>{
 
-it('render correct', ()=>{
-  expect(app).toMatchSnapshot();
-});
+  const app = shallow(<App />)
 
-it('initialize the state with empty list of gifts',()=>{
-  expect(app.state().gifts).toEqual([]);
-});
+  it('render correct', ()=>{
+    expect(app).toMatchSnapshot();
+  });
 
-it('adds a new gift to `state` when clicking the `add` gift button',()=>{
-  app.find('.btn-add').simulate('click');
-  expect(app.state().gifts).toEqual([{id: 1}]);
-});
+  it('initialize the state with empty list of gifts',()=>{
+    expect(app.state().gifts).toEqual([]);
+  });
 
-it('adds a new gift to rendered list when clicking `add gift ` button',()=>{
-  app.find('.btn-add').simulate('click');
-  expect(app.find('.gift-list').children().length).toEqual(2);
+  describe('when click the `add gift ` button',()=>{
+    beforeEach(()=>{
+      app.find('.btn-add').simulate('click');
+    })
+    afterEach(()=>{
+      app.setState({gifts:[]});
+    })
+    it('adds a new gift to `state`  ',()=>{
+      expect(app.state().gifts).toEqual([{id: 1}]);
+    });
+
+
+    it('adds a new gift to rendered list ',()=>{
+      expect(app.find('.gift-list').children().length).toEqual(1);
+    });
+  })
+
+
 });
